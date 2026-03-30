4e656042b3eabe958f0416b0e8fa790aee472f70
Dev Team <dev@server.local>
2026-03-30 06:59:41 +0000

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 <meta-charset = utf8> </meta-charset>
 <html>
     <head>
-        <title>Jeu php</title>
+        <title>PIXEL BLASTER ADVENTURE 2</title>
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?>
@@ -16,3 +16,4 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 </html>
 <script> src="maindos/main.js"</script>
 <script> src="maindos/AppBundle/main.js"</script>
+
