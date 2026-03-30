abc5bffb9f00b865222b25341233ebfa1b8c81a3
Dev Team <dev@server.local>
2026-03-30 22:30:08 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/DB/db_connect.php
+++ Backend/DB/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // On utilise l'hôte spécifique au réseau de l'IUT
-$serveur = "localhost"; 
+$serveur = "127.0.0.1"; 
 // SI localhost ne marche pas, essaie : "mariadb" ou "mysql" 
 // selon si tu es dans un conteneur spécifique du département.
 
@@ -9,7 +9,7 @@ $mot_de_passe = "Exoo2zoa";
 $base_de_donnees = "db_grp1";
 
 try {
-    $conn = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
+    $conn = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
     // Si on arrive ici, c'est que c'est gagné !
 } catch (mysqli_sql_exception $e) {
     echo "Détail de l'erreur : " . $e->getMessage();
