783602a168210a1f318bdf04262b7e74e596ff22
Dev Team <dev@server.local>
2026-03-30 06:54:41 +0000

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+?>
 <!DOCTYPE html>
 <meta-charset = utf8> </meta-charset>
 <html>
     <head>
-        <title>PHP Test</title>
+        <title>Jeu php</title>
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?>
     </body>
 </html>
+<script> src="maindos/main.js"</script>
+<script> src="maindos/AppBundle/main.js"</script>
