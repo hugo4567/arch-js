e0553157785d0a06b91201903c052c40e9c62669
Dev Team <dev@server.local>
2026-03-31 14:39:06 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/DB/db_connect.php
+++ Backend/DB/db_connect.php
@@ -15,8 +15,7 @@ try {
     $conn = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
     mysqli_set_charset($conn, "utf8");
     // Si on arrive ici, c'est que c'est gagné !
-    // essaie avec l1.dptingo-usmb.fr : connection refused.
-    // G -> connexion bloquée par le pare-feu.
+
 } catch (mysqli_sql_exception $e) {
     echo "Détail de l'erreur : " . $e->getMessage();
 }
