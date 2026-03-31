3728bac2775d21a1244f67cd26dc918355b3988f
Dev Team <dev@server.local>
2026-03-31 14:14:04 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/DB/db_connect.php
+++ Backend/DB/db_connect.php
@@ -12,7 +12,7 @@ $mot_de_passe = "Exoo2zoa";
 $base_de_donnees = "db_grp1";
 
 try {
-    $conn = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
+    $conn = mysqli_connect("l1.dptinfo-usmb.fr", $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
     mysqli_set_charset($conn, "utf8");
     // Si on arrive ici, c'est que c'est gagné !
 } catch (mysqli_sql_exception $e) {
