e864da5d377e148a559b71ff201cf4a3b3b8640f
Dev Team <dev@server.local>
2026-03-31 14:04:04 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/DB/db_connect.php
+++ Backend/DB/db_connect.php
@@ -1,4 +1,7 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 // On utilise l'hôte spécifique au réseau de l'IUT
 $serveur = "127.0.0.1"; 
 // SI localhost ne marche pas, essaie : "mariadb" ou "mysql" 
@@ -9,8 +12,10 @@ $mot_de_passe = "Exoo2zoa";
 $base_de_donnees = "db_grp1";
 
 try {
-    $conn = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
+    $conn = mysqli_connect("localhost", $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
+    mysqli_set_charset($conn, "utf8");
     // Si on arrive ici, c'est que c'est gagné !
+    // avec localhost : erreur : no such file in directory.
 } catch (mysqli_sql_exception $e) {
     echo "Détail de l'erreur : " . $e->getMessage();
 }
