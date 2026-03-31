7f93297e9929e0b6fb967f534ca4f40e0a787810
Dev Team <dev@server.local>
2026-03-31 14:19:05 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/DB/db_connect.php
+++ Backend/DB/db_connect.php
@@ -12,9 +12,11 @@ $mot_de_passe = "Exoo2zoa";
 $base_de_donnees = "db_grp1";
 
 try {
-    $conn = mysqli_connect("l1.dptinfo-usmb.fr", $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
+    $conn = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees, 3306);
     mysqli_set_charset($conn, "utf8");
     // Si on arrive ici, c'est que c'est gagné !
+    // essaie avec l1.dptingo-usmb.fr : connection refused.
+    // G -> connexion bloquée par le pare-feu.
 } catch (mysqli_sql_exception $e) {
     echo "Détail de l'erreur : " . $e->getMessage();
 }
