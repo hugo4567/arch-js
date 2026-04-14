3db5a40d33e6cf4976a498c48de23e5847d6505a
Dev Team <dev@server.local>
2026-04-14 17:05:39 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- scoreboards.php
+++ scoreboards.php
@@ -3,7 +3,8 @@ session_start();
 // Debugging (à enlever une fois le site en ligne)
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
-require_once __DIR__ . '/backend/CRUD/user.crud.php'
+require_once __DIR__ . '/Backend/CRUD/user.crud.php';
+require_once __DIR__ . '/Backend/DB/db_connect.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,7 +12,7 @@ require_once __DIR__ . '/backend/CRUD/user.crud.php'
     <meta charset="utf-8">
     <title>VOS TOPS SCORES</title>
     <link rel="stylesheet" href="style.css"> </head>
-<body>
+<body class="page-scoreboard">
 
 <?php
 $text = "<table id='scores' class='scores'>";
@@ -20,5 +21,9 @@ $text .=    "<tr><td>HUGO</td><td>12000</td></tr>";
 $text .=    "<tr><td>MAXIME</td><td>-390</td></tr>";
 $text .=  "</table>";
 echo($text);
-echo(get_all_users())
+echo(get_all_users($conn))
+?>
+
+<?php
+require_once __DIR__ . "Backend/DB/db_disconnect.php";
 ?>
