dceadf915c593750e7fb714003fe2a9096a54b0a
Dev Team <dev@server.local>
2026-04-22 07:58:07 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/views/form.view.php
+++ Backend/views/form.view.php
@@ -46,9 +46,6 @@ $note_neg = $is_edit ? $level_data['note_neg'] : 0;
         <label>Notes Négatives :</label>
         <input type="number" name="note_neg" value="<?= $note_neg ?>" required>
 
-        <label>Niveau (json) : </label>
-        <input type="file", name="niveau_json" required>
-
         <button type="submit">Enregistrer</button>
     </form>
 </body>
