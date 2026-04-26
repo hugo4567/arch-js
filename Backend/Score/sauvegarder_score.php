e314f543811881ea9c97dd3cc62f730b4e166e26
Dev Team <dev@server.local>
2026-04-26 21:17:43 +0200

    Auto-save: /etc/systemd/system-8320b7602ea74a8d80bbe8b323cb5d83-/run/credentials/git-auto-commit.service chqbrebqs::/var/lib

--- Backend/Score/sauvegarder_score.php
+++ Backend/Score/sauvegarder_score.php
@@ -4,8 +4,10 @@ if (isset($_POST['coins']) && isset($_SESSION['id_user'])){
 
     include("../CRUD/user.crud.php");
 
-    $piecesUser = get_pieces_user($conn, $id_user);
+    $res = add_pieces_user($conn, $_SESSION['id_user'], $totalCoins);
 
-    $res = add_pieces_user($conn, $_SESSION['id_user'], $totalCoins)
+    if (!$res){
+        echo "Erreur lors de l'insertion dans la base de donnée.";
+    }
 }
 ?>
